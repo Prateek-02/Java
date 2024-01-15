c241c513d50a23b3dce1620819e92e7ae042f621
Prateek <Prateekrajgrd74@gmail.com>
2024-01-15 19:44:34 +0530

    Online.java

--- Java coding/src/Online.java
+++ Java coding/src/Online.java
@@ -1,7 +1,24 @@
-public class Online {
+/*L1*/
+
+/*  public class Online {
+//     public static void main(String[] args) {
+//         System.out.println(1+2);
+//     }
+ }*/
+
+ public class Online {
+ 
     public static void main(String[] args) {
-        System.out.println(1+2);
+        int ex1 = 1+4+6;
+        int ex2 = 3+8+4;
+
+        System.out.println(ex1+ex2);
+
     }
-}
+ }
+
+
+
+
 
 
