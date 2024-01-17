3a07fa0dec9462a371d2c4ceba26ccb988b26309
Prateek <Prateekrajgrd74@gmail.com>
2024-01-17 12:18:52 +0530

    L2.java

--- Java coding/src/L2.java
+++ Java coding/src/L2.java
@@ -5,6 +5,7 @@ public class L2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        /* 
         int age = sc.nextInt();
 
         if(age>=18){
@@ -12,6 +13,33 @@ public class L2 {
         }
         else{
             System.out.println("Minor");
+        }
+        */
+
+
+        /* 
+        int num = sc.nextInt();
+
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        */
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a==b){
+            System.out.println("Equal");
+        }
+        else if(a>b){
+            System.out.println("A is greater");
+        }
+        else{
+            System.out.println("B is greater");
         }
     }
 }
