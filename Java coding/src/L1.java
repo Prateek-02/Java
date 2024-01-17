5a00b8d38e49c53a8ae00b3885be983811541781
Prateek <Prateekrajgrd74@gmail.com>
2024-01-17 11:13:49 +0530

    L1.java

--- Java coding/src/L1.java
+++ Java coding/src/L1.java
@@ -18,3 +18,20 @@ public class L1 {
 
 /*Variables in java */
 
+public class L1 {
+
+    public static void main(String[] args){
+        String name = "Tony Stark";
+        int age = 48;
+        double price = 87.98;
+        int a = 87;
+        int b = 66;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
