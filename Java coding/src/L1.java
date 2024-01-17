2ae0b181e278537b4a883b2c3ba16d29e14b7020
Prateek <Prateekrajgrd74@gmail.com>
2024-01-17 11:06:13 +0530

    L1.java

--- Java coding/src/L1.java
+++ Java coding/src/L1.java
@@ -1,5 +1,20 @@
+/* 
 public class L1 {
     public static void main(String[] args) {
-        System.out.println("good evening");
+        System.out.println("Hello world with java");
+        System.out.print("Hello world with java\nfrom apni kaksha");
     }
 }
+*/
+
+/* 
+public class L1 {
+
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****");
+    }
+}
+*/
+
+/*Variables in java */
+
