10f3583fb7b99d1e1abaffa5eb28659a02a390a2
Prateek <Prateekrajgrd74@gmail.com>
2024-01-20 01:41:33 +0530

    loops.java

--- Java coding/src/loops.java
+++ Java coding/src/loops.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class loops {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n");
+        System.out.println("Enter the value of n: ");     // To print in the new line
+        // System.out.print("Enter the value of n: ");     // To print in the same line
         int n = sc.nextInt();
         for(int i=0;i<=n;i++){                         // if we not give the terminatig condition the loop will run infinitely
             // System.out.println("Hello World!");
-            System.out.println(i);
+            System.out.println(i+" ");
+            // System.out.print(i+" ");
         }
     }
 }
