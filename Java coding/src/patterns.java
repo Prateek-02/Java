ff1a7b118ff7ce07acc9bd39bc50e8bbb7016727
Prateek <Prateekrajgrd74@gmail.com>
2024-01-21 15:35:05 +0530

    patterns.java

--- Java coding/src/patterns.java
+++ Java coding/src/patterns.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 // 1
 
+/*
 public class patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,3 +16,32 @@ public class patterns {
         }
     }
 }
+*/
+
+
+// 2 
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //Outer loop
+        for(int i=1;i<=n;i++){
+            //Inner loop
+            for(int j=1;j<=n+1;j++){
+                //cell ->(i,j)
+                if(i==1 || j==1 || i==n || j==n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
