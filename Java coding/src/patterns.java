0edee713424dcb305112931f4cb0a2aa50fc662a
Prateek <Prateekrajgrd74@gmail.com>
2024-01-21 15:39:02 +0530

    patterns.java

--- Java coding/src/patterns.java
+++ Java coding/src/patterns.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-// 1
+// 1 (Solid rectangle)
 
 /*
 public class patterns {
@@ -19,8 +19,9 @@ public class patterns {
 */
 
 
-// 2 
+// 2 (Hollow rectangle)
 
+/*
 public class patterns {
 
     public static void main(String[] args) {
@@ -38,6 +39,25 @@ public class patterns {
                 else{
                     System.out.print(" ");
                 }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+// 3 (Half pyramid)
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
