c38c6d37901b78b12d238e76d767382d05b01fb8
Prateek <Prateekrajgrd74@gmail.com>
2024-01-22 23:58:01 +0530

    adv_patterns.java

--- Java coding/src/adv_patterns.java
+++ Java coding/src/adv_patterns.java
@@ -45,6 +45,7 @@ public class adv_patterns {
 
 // 2 (Solid rhombus)
 
+/*
 public class adv_patterns {
 
     public static void main(String[] args) {
@@ -56,6 +57,94 @@ public class adv_patterns {
                 System.out.print(" ");
             }
             for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+// 3 (Numbers pyramid)
+
+/*
+public class adv_patterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+// 4 (Palindromic pattern)
+
+/*
+public class adv_patterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //1st half numbers
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            //2nd half numbers
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+// 5 (Diamond pattern)
+
+public class adv_patterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //upper half
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            int stars = (2*i)-1;
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            int stars = (2*i)-1;
+            for(int j=1;j<=stars;j++){
                 System.out.print("*");
             }
             System.out.println();
