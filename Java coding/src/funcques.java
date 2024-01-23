f05b037db62579f8520ef709d47987048d3febfe
Prateek <Prateekrajgrd74@gmail.com>
2024-01-24 01:59:49 +0530

    funcques.java

--- Java coding/src/funcques.java
+++ Java coding/src/funcques.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-// 1(print the avg of three numbers)
+// 1(Print the avg of three numbers)
 
 /*
 public class funcques {
@@ -25,3 +25,45 @@ public class funcques {
 
 // 2(Print the sum of all odd numbers from 1 to n)
 
+/*
+public class funcques {
+
+    public static void odd(int n){
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        odd(n);
+    }
+}
+*/
+
+
+// 3(Take two numbers and finid the greatest)
+
+public class funcques {
+
+    public static void greatest(int a,int b){
+        if(a>b){
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        greatest(a, b);
+    }
+}
