85cca188f0a8754848e910338407fe02974ba234
Prateek <Prateekrajgrd74@gmail.com>
2024-01-24 01:48:24 +0530

    funcques.java

---
+++ Java coding/src/funcques.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+// 1(print the avg of three numbers)
+
+/*
+public class funcques {
+
+    public static void avg(int a,int b,int c){
+        int sum = a+b+c;
+        int avg = sum/3;
+        System.out.println("Avg of numbers: " +avg);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        avg(a,b,c);
+    }
+}
+*/
+
+
+// 2(Print the sum of all odd numbers from 1 to n)
+
