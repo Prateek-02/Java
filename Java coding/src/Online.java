313268190a582fdd7ea7066a7c5d9e3a5093034d
Prateek <Prateekrajgrd74@gmail.com>
2024-01-30 20:17:33 +0530

    Online.java

--- Java coding/src/Online.java
+++ Java coding/src/Online.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 /*L1*/
 
 /*  public class Online {
@@ -20,8 +23,22 @@
 //     }
 //  }
 
-// 3
+/* ARRAY */
 
+/**
+ * Online
+ */
+public class Online {
+
+    public static void main(String[] args){
+        int arr[] = {1,2,3};
+        int n = arr.length;
+        System.out.println("Length of thr array is: " + n);
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
 
 
 
