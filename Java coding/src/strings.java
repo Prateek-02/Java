08a276dba23539031b6ffba78e4202328d959d3d
Prateek <Prateekrajgrd74@gmail.com>
2024-02-03 23:43:04 +0530

    strings.java

--- Java coding/src/strings.java
+++ Java coding/src/strings.java
@@ -184,5 +184,12 @@ public class strings {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Tony");
         System.out.println(sb);
+
+        // char at index 0
+        System.out.println(sb.charAt(0));
+
+        // set char at index
+        sb.setCharAt(0, 'P');
+        System.out.println(sb);
     }
 }
