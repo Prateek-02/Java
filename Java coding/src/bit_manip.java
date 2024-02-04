feba5f073666d6fbde02a29533420b2a4d8f1154
Prateek <Prateekrajgrd74@gmail.com>
2024-02-05 01:39:00 +0530

    bit_manip.java

--- Java coding/src/bit_manip.java
+++ Java coding/src/bit_manip.java
@@ -1,15 +1,33 @@
+import java.util.*;
+
+// Get Bit
+
+/*
 public class bit_manip {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5;  // 0101
         int pos = 2;
         int bitMask = 1<<pos;
 
-        if((bitMask & n) == 0){
+        if((bitMask & n) == 0){      // And operation is used to get the bit
             System.out.println("Bit was zero");
         }
         else{
             System.out.println("Bit was one");
         }
-        
+    }
+}
+*/
+
+// Set Bit  (OR operation is used to set the bit)
+
+public class bit_manip {
+    public static void main(String[] args) {
+       int n = 5;
+       int pos = 1;
+       int bitMask = 1<<pos;
+
+       int newNum = bitMask | n;
+       System.out.println(newNum);
     }
 }
