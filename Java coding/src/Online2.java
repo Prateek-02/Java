248b2ab25897759006be999f378d05eb8ea10cee
Prateek <Prateekrajgrd74@gmail.com>
2024-02-13 20:40:53 +0530

    13feb

--- Java coding/src/Online2.java
+++ Java coding/src/Online2.java
@@ -1,5 +1,26 @@
 import java.util.*;
+@SuppressWarnings("unused")
+
+
+/* CLASS AND OBJECTS */
 
 public class Online2 {
-    
+    //attributes(instance properties)
+    int a =5;
+    int b =6;
+
+    public static void main(String[] args){
+        Online2 obj = new Online2();
+        int x = obj.a;
+        int y = obj.b;
+        int z = x+y;
+        System.out.println("Sum of x+y: "+z);
+
+        x += 5; 
+        y += 5;
+        z = x+y;
+        System.out.println("Sum of x+y: "+z);
+    }
+
+
 }
