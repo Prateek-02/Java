2e946d042da1e30ef89dc42a0a2e290c07c5bed4
Prateek <Prateekrajgrd74@gmail.com>
2024-02-15 20:24:50 +0530

    15feb.java

--- Java coding/src/Sorting.java
+++ Java coding/src/Sorting.java
@@ -14,6 +14,7 @@ public class Sorting {
         for(int i=0;i<size-1;i++){
             for(int j=0;j<size-i-1;j++)
             if(arr[j]>arr[j+1]){
+                //swap
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
