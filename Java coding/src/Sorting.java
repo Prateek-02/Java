fc8da026a3bc9a1fcfb893d6c5c1d7680425aff5
Prateek <Prateekrajgrd74@gmail.com>
2024-02-15 20:16:55 +0530

    15feb.java

---
+++ Java coding/src/Sorting.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+// 1) Bubble sort
+
+public class Sorting {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<size-1;i++){
+            for(int j=0;j<size-i-1;j++)
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
+   } 
+}
