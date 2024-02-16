8fb92117f126204212b40c78c3f90b56432d22e8
Prateek <Prateekrajgrd74@gmail.com>
2024-02-16 09:27:01 +0530

    16ferb.java

--- Java coding/src/Sorting.java
+++ Java coding/src/Sorting.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
-// 1) Bubble sort
+// 1) Bubble sort (Time complexity = O(n^2))
 
 public class Sorting {
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
         int size = sc.nextInt();
         int arr[] = new int[size];
+        System.out.print("Enter the elements of array: ");
         for(int i=0;i<size;i++){
             arr[i] = sc.nextInt();
         }
@@ -14,13 +16,14 @@ public class Sorting {
         for(int i=0;i<size-1;i++){
             for(int j=0;j<size-i-1;j++)
             if(arr[j]>arr[j+1]){
-                //swap
+                //swap  
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
-
+        
+        System.out.print("Sorted array: ");
         for(int i=0;i<size;i++){
             System.out.print(arr[i]+" ");
         }
