d73fa62585bf3d40364955967527b77c42fcbc17
Prateek <Prateekrajgrd74@gmail.com>
2024-03-18 20:49:23 +0530

    19thmar.java

--- Java coding/src/Online3.java
+++ Java coding/src/Online3.java
@@ -1,3 +1,36 @@
-public class Online3 {
+interface A {
+    void methodA();
+}
+
+interface B extends A {
+    void methodB();
+}
+
+interface C {
+    void methodC();
+}
+
+class Test implements B, C {
     
+    public void methodA() {
+        System.out.println("Inside methodA");
+    }
+
+    public void methodB() {
+        System.out.println("Inside methodB");
+    }
+
+    public void methodC() {
+        System.out.println("Inside methodC");
+    }
+}
+
+public class Online3 {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        test.methodA();
+        test.methodB();
+        test.methodC();
+    }
 }
