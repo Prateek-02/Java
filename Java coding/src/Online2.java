c07a282718ac38bebf50dd7da1866e2932cbff9c
Prateek <Prateekrajgrd74@gmail.com>
2024-03-18 20:18:25 +0530

    19thmar.java

--- Java coding/src/Online2.java
+++ Java coding/src/Online2.java
@@ -185,6 +185,9 @@ public class Online2 {
 }
 */
 
+//Write a Java program to create an interface Drawable with a method draw() that takes no arguments and returns void.
+// Create three classes Circle, Rectangle, and Triangle that implement the Drawable interface.
+//override the draw() method to draw their respective shapes.
 
 interface Drawable {
     void draw();
