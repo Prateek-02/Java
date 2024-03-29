8fb1db4c133891185e4fbe7db5ee2b4e712a455f
Prateek <Prateekrajgrd74@gmail.com>
2024-03-29 10:36:26 +0530

    29mar.java

---
+++ Java coding/src/Recursion1.java
@@ -0,0 +1,137 @@
+import java.util.Scanner;
+
+// 1 (Print 5 to 1)
+/*
+public class Recursion1 {
+    public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        printNum(n);
+
+        sc.close();
+    }
+}
+*/
+
+
+// 2 (Print 1 to 5)
+/*
+public class Recursion1 {
+    public static void printNum(int n){
+        if(n==6){
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        printNum(n);
+
+        sc.close();
+    }
+}
+*/
+
+
+// 3 (Print Sum of n Natural numbers)
+
+/*
+public class Recursion1 {
+
+    public static void printSum(int i,int n,int sum){
+        if(i==n){
+            sum +=i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        printSum(i+1, n, sum);
+        System.out.println(i);
+    }
+    public static void main(String[] args) {
+        printSum(1,5,0);
+    }
+}
+*/
+
+
+// 4 (Print Factorial of a number)
+
+/*
+public class Recursion1 {
+    public static int printFact(int n){
+        if(n==0 || n==1){
+           return 1; 
+        }
+        int x = printFact(n-1);
+        int fact = n*x;
+        return fact;
+    }
+    public static void main(String[] args) {
+       int n = 5;
+       int ans = printFact(n);
+       System.out.println(ans);
+    }
+}
+*/
+
+
+// 5 (Print the fibonacci series till nth term)
+
+/*
+public class Recursion1 {
+    public static void printFibo(int a,int b,int n){
+        if(n==0){
+            return;
+        }
+        int c = a+b;
+        System.out.print(c+" ");
+        printFibo(b, c, n-1);
+
+    }
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.print(a+" "+b+" ");
+        int n = 7;
+        printFibo(a, b, n-2);
+    }
+    
+}
+*/
+
+
+// 6 (Print x^n)
+
+
+public class Recursion1 {
+    public static int printPower(int x,int n){
+        if(n==0){
+            return 1;
+        } 
+        if(x==0){
+            return 0;
+        } 
+        int xPownm1 = printPower(x, n-1);
+        int xPown = x*xPownm1;
+        return xPown;  
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 6;
+        int ans = printPower(x, n);
+        System.out.println(ans);
+
+    }
+}
