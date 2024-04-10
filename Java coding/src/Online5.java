12ab3adbb3a503b5952bb99df4dc21addfc390dd
Prateek <Prateekrajgrd74@gmail.com>
2024-04-10 19:54:06 +0530

    10apr.java

---
+++ Java coding/src/Online5.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Online5 {
+    public static void main(String[] args) {
+        /* 
+        Create File
+
+        File file = new File("test.txt");
+
+        try{
+            System.out.println(file.createNewFile());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error");
+        }
+        */
+
+        /*
+        Read File
+        char arr[] = new char[100];
+
+        try{
+            FileReader fileReader = new FileReader("test.txt");
+            fileReader.read(arr);
+            System.out.println(arr);
+
+            fileReader.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        */
+
+        // Write into File
+        String data = "New data added to file";
+
+        try{
+            FileWriter fileWriter = new FileWriter("test.txt");
+            fileWriter.write(data);
+
+            fileWriter.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // Delete File
+
+        File file = new File("test.txt");
+
+        try{
+            System.out.println(file.delete());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error");
+        }
+    } 
+}
