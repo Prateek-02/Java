8abd0acc5af76e01420e4f02f485f4a570983b90
Prateek <Prateekrajgrd74@gmail.com>
2024-04-24 22:58:26 +0530

    24apr.html

--- Java coding/src/FileHandling.java
+++ Java coding/src/FileHandling.java
@@ -1,11 +1,12 @@
 import java.io.*;
-// import java.util.Scanner;
+import java.util.Scanner;
 
+// 1
+/*
 public class FileHandling {
     public static void main(String[] args) {
 
         // Code to create a new file
-        /* 
         File myFile =  new File("test.txt");
         try{
             myFile.createNewFile();
@@ -42,7 +43,7 @@ public class FileHandling {
         catch(FileNotFoundException e){
             e.printStackTrace();
         }
-        */
+        
 
 
         // Deleting a file
@@ -56,3 +57,62 @@ public class FileHandling {
         }
     }
 }
+*/
+
+
+// 2
+
+public class FileHandling {
+    public static void main(String[] args) {
+        /*
+        File myfile = new File("test2.txt");
+        try{
+            myfile.createNewFile();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        
+
+        // Write into file
+
+        try{
+            FileWriter myfile = new FileWriter("test2.txt");
+            myfile.write("Namste Duniyaa!!!");
+            myfile.close();
+        }
+        
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        
+
+        // Read from file
+
+        File myfile = new File("test2.txt");
+        try{
+            Scanner sc = new Scanner(myfile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        */
+
+        // Deleting a file
+
+        File myFile = new File("test2.txt");
+        if(myFile.delete()){
+            System.out.println("File: "+myFile.getName()+" deleted");
+        }
+        else{
+            System.out.println("Some error occured");
+        }
+
+    }
+}
+
